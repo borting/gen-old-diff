55ad285fbc258e27eb554fc5c5b65422cb386049
borting <bortingchen@gmail.com>
2020-01-01 18:49:52 +0800

    Parse the results of git diff

--- bash/create-diff-dirs.sh
+++ bash/create-diff-dirs.sh
@@ -3,6 +3,8 @@
 #	$1:	SHA-1 ID of commit 1
 #	$2:	SHA-1 ID of commit 2
 #	$3: folder for storing diff files
+# Types of git diff result
+#	https://git-scm.com/docs/git-diff#Documentation/git-diff.txt---diff-filterACDMRTUXB82308203
 
 ERR_VCS=1
 ERR_INPUT=2
@@ -31,4 +33,27 @@ if !(mkdir -p $3 &> /dev/null); then
 	exit ${ERR_INPUT}
 fi
 
-echo "Alles Gut!"
+# Obtain git diff results between two commits
+DIFF_RESULTS=`git diff --name-status ${1} ${2}`
+#DIFF_RESULTS=`git diff -C --name-status ${1} ${2}`
+
+while IFS=$'\t' read -r -a DIFF_FILE ; do
+	DIFF_TYPE=${DIFF_FILE[0]}
+	case "${DIFF_TYPE::1}" in
+		M)	# Modified
+			echo "${DIFF_FILE[1]} is modified";;
+		R)	# Renamed
+			echo "Move ${DIFF_FILE[1]} to ${DIFF_FILE[2]}";;
+		D)	# Deleted
+			echo "${DIFF_FILE[1]} is deleted";;
+		C)	# Copied
+			echo "Copy ${DIFF_FILE[1]} to ${DIFF_FILE[2]}";;
+		A)	# Added
+			echo "${DIFF_FILE[1]} is added";;
+		T)
+			echo "${DIFF_FILE[1]} is changed";;
+		*)
+			echo -e "Unknown diff type:\t${DIFF_FILE[0]}\t${DIFF_FILE[1]}\t${DIFF_FILE[2]}"
+	esac
+done <<< "${DIFF_RESULTS}"
+
