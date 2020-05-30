b0cb46465ec962d312d6fa1bae120f1f88f088d5
borting <bortingchen@gmail.com>
2020-05-30 17:59:12 +0800

    Use pathlib.Path on dir/file operation

--- python/gen-old-diff.py
+++ python/gen-old-diff.py
@@ -1,33 +1,26 @@
 #!/usr/bin/env python3
 
 import git
-import os
 import stat
 import sys
 from pathlib import Path
 
 def get_file_from_blob(mode, blob, path):
-    # Create sub-dir
-    if not os.path.exists(os.path.dirname(path)):
-        try:
-            os.makedirs(os.path.dirname(path))
-        except OSError as exc: # Guard against race condition
-            if exc.errno != errno.EEXIST:
-                raise
+    # Create sub-dir is necessary
+    path.parents[0].mkdir(parents=True, exist_ok=True)
 
     # Write blob content to file
-    f = open(path, "wb")
-    blob.stream_data(f)
-    f.close()
-
+    with path.open(mode="wb") as f:
+        blob.stream_data(f)
+        
     # Change file permission
-    os.chmod(path, mode & (stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO))
+    path.chmod(mode & 0o777)
 
 def gen_diff_files(diffIdx, dir_old, dir_new, gen_old=None, gen_new=None):
     if gen_old:
-        get_file_from_blob(diffIdx.a_mode, diffIdx.a_blob, dir_old + diffIdx.a_path)
+        get_file_from_blob(diffIdx.a_mode, diffIdx.a_blob, Path(dir_old + diffIdx.a_path))
     if gen_new:
-        get_file_from_blob(diffIdx.b_mode, diffIdx.b_blob, dir_new + diffIdx.b_path)
+        get_file_from_blob(diffIdx.b_mode, diffIdx.b_blob, Path(dir_new + diffIdx.b_path))
 
 def check_output(path):
     out_file = path.name
@@ -44,8 +37,7 @@ if __name__ == "__main__":
         print("RUN: gen-old-diff.py PATH_TO_OUTPUT_FILE COMMIT_1 [COMMIT_2]")
 
     # FIXME: check cwd is a git repo
-    cwd = os.getcwd()
-    repo = git.Repo(cwd)
+    repo = git.Repo(Path().absolute())
 
     # Parse and check output directoy and output file name
     out_dir, out_file = check_output(Path(sys.argv[1]).absolute())
