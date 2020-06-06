984424b362b0746f8cbbebc9dbb0c04c71e36082
borting <bortingchen@gmail.com>
2020-06-06 20:26:32 +0800

    Check output file extension and compression method

--- python/gen-old-diff.py
+++ python/gen-old-diff.py
@@ -10,6 +10,7 @@ import gitdb
 import stat
 import sys
 import tempfile
+from enum import IntEnum
 from pathlib import Path
 
 def get_file_from_blob(mode, blob, path):
@@ -30,12 +31,56 @@ def gen_diff_files(diffIdx, dir_old, dir_new, gen_old=None, gen_new=None):
     if gen_new:
         get_file_from_blob(diffIdx.b_mode, diffIdx.b_blob, dir_new.joinpath(diffIdx.b_path))
 
+class OutExt(IntEnum):
+    DIR = 1
+    TGZ = 2
+    TXZ = 4
+    TBZ2 = 8
+    TZ = 16
+    ZIP = 32
+    P7Z = 64
+    RAR = 128
+    ERR = 0
+
+class UnsupportCompressException(Exception):
+    def __init__(self, msg):
+        super().__init__(msg)
+
+class UnknownCompressException(Exception):
+    def __init__(self, msg):
+        super().__init__(msg)
+
 def check_output(path):
     # Check if parent dir exists
     # TODO: use try ... except (FileExistsError, FileNotFoundError)
     if path.parents[0].exists():
         path.parents[0].mkdir(parents=True, exist_ok=True)
-    return path.parents[0]
+
+    # Parse file extension
+    suffixes = "".join(path.suffixes)
+    out_ext = {
+        "": OutExt.DIR,
+        ".tar.gz": OutExt.TGZ,
+        ".tgz": OutExt.TGZ,
+        ".tar.xz": OutExt.TXZ,
+        ".txz": OutExt.TXZ,
+        ".tar.bz2": OutExt.TBZ2,
+        ".tbz2": OutExt.TBZ2,
+        ".tar.Z": OutExt.TZ,
+        ".zip": OutExt.ZIP,
+        ".7z": OutExt.P7Z,
+        ".rar": OutExt.RAR
+    }.get(suffixes, OutExt.ERR)
+
+    # Unknown compression file extension
+    if out_ext == OutExt.ERR:
+        raise UnknownCompressException(suffixes)
+
+    # Unsupported compression method
+    if out_ext & (OutExt.TZ | OutExt.P7Z | OutExt.RAR):
+        raise UnsupportCompressException(suffixes)
+
+    return out_ext
 
 class CommitIdException(Exception):
     def __init__(self, msg):
@@ -80,9 +125,10 @@ if __name__ == "__main__":
         print("Error: {} is not a valid commit ID".format(err))
         sys.exit(1)
 
-    # Parse and check output directoy and output file name
+    # Check output parent directoy and compression method
     out_file = Path(sys.argv[1]).resolve()
-    check_output(out_file)
+    out_ext = check_output(out_file)
+    print(out_ext)
 
     with tempfile.TemporaryDirectory() as tmp_dir:
         dir_old = Path(tmp_dir + "/old/")
